7a93ac66a911b7006bbd7ce9984e63aaf896b37e
Terrance-Nightingale <130939937+Terrance-Nightingale@users.noreply.github.com>
2023-09-27 16:35:32 -0600

    Stop progress bar upon any key press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+
 # TODO Add a text entry box for typing
 # TODO Add a 5 second timer that resets back to 5 whenever the user types
 #     -This will be a bar that updates in length and turns more red as time passes.
@@ -7,11 +8,20 @@ from tkinter import ttk
 BAR_COLOR = '#cc2342'
 TROUGH_COLOR = '#ccc8c9'
 SECONDS = 5 * 10
-
 window = Tk()
 window.title("Disappearing Text")
 window.minsize(width=600, height=700)
 window.config(padx=50, pady=20)
+
+
+def stop_progress(self):
+    progressbar.stop()
+    progressbar.config(value=0)
+
+
+def start_progress():
+    progressbar.start()
+
 
 s = ttk.Style()
 s.theme_use('clam')
@@ -21,10 +31,14 @@ s.configure("red.Horizontal.TProgressbar", troughcolor=TROUGH_COLOR, darkcolor=B
 progressbar = ttk.Progressbar(window, style="red.Horizontal.TProgressbar", length=500, maximum=100, value=0)
 progressbar.step(0.5)
 progressbar.pack()
+progressbar.start()
+
+text_box = Text()
+text_box.bind_all('<Key>', stop_progress)
+text_box.pack()
+
 progressbar.start(SECONDS)
 while progressbar['value'] != progressbar['maximum'] - 0.5:
     window.update()
-
-progressbar.stop()
 
 window.mainloop()
